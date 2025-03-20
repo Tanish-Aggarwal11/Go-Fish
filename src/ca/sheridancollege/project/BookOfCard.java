2cb5ee96488723cf36038a73dc9d7757b6e95a3d
Tanish Aggarwal <aggatani@sheridancollege.ca>
2025-03-20 01:30:13 -0400

    Extended the abstract classes according to Go Fish game

---
+++ src/ca/sheridancollege/project/BookOfCard.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author tanis
+ */
+public class BookOfCard {
+    
+    Card[] book = new Card[2];
+    
+    public BookOfCard(GoFishCard c1, GoFishCard c2) {
+        
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/ca/sheridancollege/project/GoFishPlayer.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author tanis
+ */
+public class GoFishPlayer extends Player {
+
+    public GoFishPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+        
+    }
+    
+}
